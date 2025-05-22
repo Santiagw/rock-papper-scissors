668408e76ce54e0d936378410d105f65fcd96ba2
santiagw <ottawa104421@gmail.com>
2025-05-22 17:13:09 -0500

    gets human choice

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let humanScore = 0
+let getComputerScore = 0
+
 function getComputerChoice(){
     const choices = ["rock", "papper", "scissors"]
     let randomNum = Math.random()
@@ -10,4 +13,14 @@ function getComputerChoice(){
     }
 }
 
-console.log(getComputerChoice())
+function getHumanChoice(){
+    let userChoice = prompt("rock, papper or scissors")
+    console.log(userChoice)
+    return userChoice
+}
+
+function PlayRound(){
+    
+}
+
+console.log(getHumanChoice())
