5555028cfd633f455ef9f820d12ac21ab8be60a6
santiagw <ottawa104421@gmail.com>
2025-05-26 18:51:44 -0500

    feat: display game results on screen

--- script.js
+++ script.js
@@ -15,38 +15,77 @@ function getHumanChoice() {
     return userChoice
 }
 
-function PlayGame() {
-    let humanScore = 0
-    let ComputerScore = 0
-    function playRound(playerSelection, computerSelection) {
-        if (playerSelection === "rock" && computerSelection === "scissors") {
-            humanScore++
-            console.log(`player scrore: ${humanScore}
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === "rock" && computerSelection === "scissors") {
+        humanScore++
+        console.log(`player scrore: ${humanScore}
             Computer score: ${ComputerScore}`)
-            return `You win ${playerSelection} Beats ${computerSelection}`;
-        }
-        else if (playerSelection === "papper" && computerSelection === "rock") {
-            humanScore++
-            console.log(`player scrore: ${humanScore}
+        humanScoreDisplay.textContent = "human: " + humanScore
+        winnerDisplay.textContent = "human wins and gets one point"
+        return `You win ${playerSelection} Beats ${computerSelection}`;
+    }
+    else if (playerSelection === "papper" && computerSelection === "rock") {
+        humanScore++
+        console.log(`player scrore: ${humanScore}
             Computer score: ${ComputerScore}`)
-            return `You win ${playerSelection} Beats ${computerSelection}`;
-        }
-        else if (playerSelection === "scissors" && computerSelection === "papper") {
-            humanScore++
-            console.log(`player scrore: ${humanScore}
-            Computer score: ${ComputerScore}`)
-            return `You win ${playerSelection} Beats ${computerSelection}`;
-        }
-        else if (playerSelection === computerSelection) return `It is a Tie!`;
-        else {
-            ComputerScore++
-            console.log(`player scrore: ${humanScore}
-            Computer score: ${ComputerScore}`)
-            return `You lose ${computerSelection} Beats ${playerSelection}`;
-        }
+        humanScoreDisplay.textContent = "human: " + humanScore
+        winnerDisplay.textContent = "human wins and gets one point"
 
+        return `You win ${playerSelection} Beats ${computerSelection}`;
+    }
+    else if (playerSelection === "scissors" && computerSelection === "papper") {
+        humanScore++
+        console.log(`player scrore: ${humanScore}
+            Computer score: ${ComputerScore}`)
+        humanScoreDisplay.textContent = "human: " + humanScore
+        winnerDisplay.textContent = "human wins and gets one point"
+
+        return `You win ${playerSelection} Beats ${computerSelection}`;
+    }
+    else if (playerSelection === computerSelection) return `It is a Tie!`;
+    else {
+        ComputerScore++
+        console.log(`player scrore: ${humanScore}
+            Computer score: ${ComputerScore}`)
+        computerScoreDisplay.textContent = "computer: " + ComputerScore
+        winnerDisplay.textContent = "computer wins and gets one point"
+
+        return `You lose ${computerSelection} Beats ${playerSelection}`;
     }
 
-
-
 }
+
+let humanScore = 0
+let ComputerScore = 0
+
+const gameContainer = document.querySelector("#container")
+const resultsContainer = document.querySelector(".results")
+
+const humanScoreDisplay = document.querySelector(".humanScore")
+const computerScoreDisplay = document.querySelector(".computerScore")
+const winnerDisplay = document.querySelector(".winner")
+
+const rockBTN = document.createElement("button")
+const papperBTN = document.createElement("button")
+const scissorsBTN = document.createElement("button")
+
+rockBTN.textContent = "rock"
+papperBTN.textContent = "papper"
+scissorsBTN.textContent = "scissors"
+
+
+//attach an event listener to every button that calls the plaround function with its respective player selection
+
+rockBTN.addEventListener("click", () => {
+    playRound("rock", getComputerChoice())
+});
+
+papperBTN.addEventListener("click", () => {
+    playRound("papper", getComputerChoice())
+});
+
+scissorsBTN.addEventListener("click", () => {
+    playRound("scissors", getComputerChoice())
+});
+
+gameContainer.append(rockBTN, papperBTN, scissorsBTN)
